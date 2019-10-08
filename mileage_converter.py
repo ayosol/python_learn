1780c1b6358448078c7e0132162030e6bf88b956
ayosol <solomonayodeleo@gmail.com>
2019-10-08 15:12:47 +0100

    Just a minor addition

--- mileage_converter.py
+++ mileage_converter.py
@@ -1,4 +1,4 @@
 print("How many kilometeres do you want to convert?")
 kilometer = float(input())
 mile = kilometer/1.60934
-print(f"The equivalent distance you travelled in miles is {roun(mile, 3)} miles")
+print(f"The equivalent distance you travelled in {kilometer}km is {round(mile, 3)}mi")
